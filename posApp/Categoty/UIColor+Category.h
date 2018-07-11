6e515672363fff206e2ca202b58fb8ec6c6e98cc
875282902 <875282902@qq.com>
2018-07-11 13:47:33 +0800

    1

---
+++ posApp/Categoty/UIColor+Category.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Category.h
+//  anjuyi1
+//
+//  Created by apple on 2018/6/13.
+//  Copyright © 2018年 lsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Category)
+
++ (UIColor *)colorWithHexString:(NSString *)color;
+
+@end
